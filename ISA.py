5989187e248996b03734db10c5c5dd5142bd604d
aisabelmoreno <31948233+aisabelmoreno@users.noreply.github.com>
2018-10-12 00:22:09 -0500

    Add files via upload
    Will be fixed in the morning.

---
+++ ISA.py
@@ -0,0 +1,24 @@
+#ISA design
+
+Opcode = {
+0000,
+0010,
+0011,
+0100,
+0101,
+0110,
+0111,
+1000,
+1000,
+1001,
+1010,
+1011,
+1100,
+1110,
+1111
+
+}
+input_file = open("P1.txt", "r")
+
+input_file.close()
+#output_file.close()
