45f19439ba8c426a12ab8f805e5e3ed8c151f282
aisabelmoreno <31948233+aisabelmoreno@users.noreply.github.com>
2018-10-17 16:49:42 -0500

    updated  files
    Isa.py has disassembler that should read in all assigned instructions outlined in our ISA Design.

--- ISA.py
+++ ISA.py
@@ -5,23 +5,98 @@ def disassembler(M,Nlines):
     print("----------------")
 	#print the instructions
     Rx = 0
+    Ry= 0
     imm = 0
-    print(" Nlines in loop: ", Nlines)
+    #print(" Nlines in loop: ", Nlines)
     for i in range(Nlines):
         #print("in loop")
         #print("i is: ", i)
         fetch =M[i]
-        print("M[",i,"]:",M[i])
+        #print("M[",i,"]:",M[i])
         #print(fetch)
-        if(fetch[0:4] == "0000"): # 
-            #print("fetch values for 0:3: ", fetch[0:4])#prints 1st 4 values
-            #print("fetch values for 4:6: ", fetch[4:6])#prints 5th and 6th value
-            Rx = int(fetch[4:6],2) #uses 5th and 6th value and changes it to dec
-            imm= int(fetch[6:8],2)  #uses 7th and 8th value and changes it to dec
-            print("loadi R" + str(Rx) +",(R" + str(imm) + ")")
-            print("Rx: ",Rx)
-        elif(fetch[0:4] == "0001"):#
-            print("another intsruction")
+        if(fetch[0:4] == "0011"): # load imm
+        #    Rx = int(fetch[4:6],2) #uses 5th and 6th value and changes it to dec
+            imm= int(fetch[4:8],2)  
+            print("load "+ str(imm))
+
+        elif(fetch[0:4] == "1000"):#store Rx, Ry Done
+            Rx= int(fetch[4:6],2)
+            Ry= int(fetch[6:8],2)
+            print("store R" + str(Rx)+ ", R" + str(Ry))
+            
+        elif(fetch[0:6] == "000100"):#srl Rx Done
+            Rx= int(fetch[6:8],2)
+            #Ry= int(fetch[6:8],2)
+            print("srl R" + str(Rx))
+            
+        elif(fetch[0:4] == "0100"):#addi Rx, imm Done
+            Rx= int(fetch[4:6],2)
+            imm= int(fetch[6:8],2)
+            print("addi R" + str(Rx)+ ", " + str(imm))
+            
+        elif(fetch[0:6] == "010110"):#subi Rx Done
+            #Rx= int(fetch[4:6],2)
+            Ry= int(fetch[6:8],2)
+            print("subi R" + str(Ry))
+            
+        elif(fetch[0:6] == "111011"):#bne Rx Done
+            Rx= int(fetch[6:8],2)
+            #Ry= int(fetch[6:8],2)
+            print("bne R" + str(Rx))
+            
+        elif(fetch[0:6] == "110001"):#slt Rx Done
+            #Rx= int(fetch[4:6],2)
+            Rx= int(fetch[6:8],2)
+            print("slt R" + str(Rx))
+            
+        elif(fetch[0:5] == "00100"):#BezDec imm 
+            #Rx= int(fetch[4:6],2)
+            imm= int(fetch[5:8],2)
+            print("bezDec " + str(imm))
+            
+        elif(fetch[0:4] == "0111"):#xori Rx, imm
+            Rx= int(fetch[4:6],2)
+            imm= int(fetch[6:8],2)
+            print("xori R" + str(Rx)+ ", R" + str(imm))
+            
+        elif(fetch[0:4] == "0111"):#andi Rx, imm
+            Rx= int(fetch[4:6],2)
+            imm= int(fetch[6:8],2)
+            print("andi" + str(Rx)+ "," + str(imm))
+            
+        elif(fetch[0:4] == "1010"):#jump 'branch'(imm)
+            imm= int(fetch[4:8],2)
+            Ry= int(fetch[6:8],2)
+            print("jump" + str(Rx)+ "," + str(imm))
+            
+        elif(fetch[0:4] == "1001"):#add RX, Ry
+            Rx= int(fetch[4:6],2)
+            Ry= int(fetch[6:8],2)
+            print("add R" + str(Rx)+ ", R" + str(Ry))
+            
+        elif(fetch[0:4] == "1101"):#sub Rx, Ry
+            Rx= int(fetch[4:6],2)
+            Ry= int(fetch[6:8],2)
+            print("sub R" + str(Rx)+ ", R" + str(Ry))
+            
+        elif(fetch[0:8] == "10110110"):#subln R4
+            #Rx= int(fetch[4:6],2)
+            #Ry= int(fetch[6:8],2)
+            print("subln $R4")
+            
+        elif(fetch[0:8] == "11110000"):#bne R4
+            #Rx= int(fetch[4:6],2)
+            #Ry= int(fetch[6:8],2)
+            print("bne $R4" )   
+        elif(fetch[0:8] == "01010101"):# jump ' first branch'
+            #Rx= int(fetch[4:6],2)
+            #Ry= int(fetch[6:8],2)
+            print("jump 'first branch' ")
+        elif(fetch[0:8] == "00000000"):#halt 
+            Rx= int(fetch[4:6],2)
+            Ry= int(fetch[6:8],2)
+            print("halt")    
+            
         else:
             print("Instruction set not supported")
 	
